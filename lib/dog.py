d07abff8eab2b6df90d5d8705073e52e4d1d9dca
Kelvin Masambu <kevohmasambu@gmail.com>
2024-11-07 12:09:12 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,41 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = 'Dog', breed = 'Beagle'):
+
+        self.set_name(name)
+        if hasattr(self, '_name'):  # Only set breed if name is valid
+            self.set_breed(breed)
+
+    def get_name(self):
+        print('Retrieving name.')
+        return self._name
+
+    def set_name(self, name):
+
+        if type(name) == str and 1 <= len(name) <= 25:
+            print(f'Setting name to {name}.')
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        print('Retrieving breed')
+        return self._breed
+
+    def set_breed(self, breed):
+        
+        if breed in APPROVED_BREEDS:
+            print(f'Setting breed to {breed}.')
+            self._breed = breed
+
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+fido = Dog('Fido','Human')
+print(fido.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'Person', job = 'Job'):
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        print('Retrieving name.')
+        return self._name 
+
+    def set_name(self, name):
+        
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+
+        else:
+            self._name = name.strip().title()
+
+    def get_job(self):
+        print('Retrieving job.')
+        return self._job
+
+    def set_job(self, job):
+        
+        if job not in APPROVED_JOBS:
+            print('Job must be in list of approved jobs.')
+            self._job = job 
+
+        else:
+            self._job = job 
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
